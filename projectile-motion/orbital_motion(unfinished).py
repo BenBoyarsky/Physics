655a7fbf44e9231513f175c58b4d421e2c6089a8
Benjamin <benjaminboyarsky21@gmail.com>
2026-08-08 17:50:16 -0700

    Added orbital motion file, and cleaned up syntax

---
+++ projectile-motion/orbital_motion(unfinished).py
@@ -0,0 +1,21 @@
+#orbital motion around a fixed planet
+#potential updates: user doesn't start from a fixed location; 3D coordinates instead of 2D;
+#                   add atmosphere; user may select planets instead of inputting their own parameters;
+#                   figure out system for dealing with infinite loops (i.e. user cannot exceed escape v)
+
+import numpy as np
+import sys
+
+def final_pos(planet_radius, planet_mass, object_mass, initial_speed, angle, height):
+    pass
+
+def main():
+    R = float(input('Planet Radius: '))
+    M = float(input('Planet Mass: '))
+    m = float(input('Object Mass: '))
+    v = float(input('Initial Speed: '))
+    h = float(input('Initial Height: '))
+    final_pos(R, M, m, v, h)
+
+if __name__ == '__main__':
+    main()
--- projectile-motion/projectile_motion(flat_ground).py
+++ projectile-motion/projectile_motion(flat_ground).py
@@ -1,5 +1,8 @@
 #projecile motion on flat ground with numerical methods
 #potential updates: return final velocity vector; return final angle; calculate g based on height, not fixed;
+#                   add aerodynamics assuming a spherical object; add multiple types of objects (This has to
+#                   be a different update because effective surface area changes with an objects angle in the air);
+#                   3D instead of 2D;
 
 import numpy as np
 import sys
