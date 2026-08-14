425f43edf75117ce2851a987994e3de74ad40666
Benjamin <benjaminboyarsky21@gmail.com>
2026-08-14 14:36:26 +0000

    simulate() in charge problem

--- projectile-motion/2-charge_problem.py
+++ projectile-motion/2-charge_problem.py
@@ -1,5 +1,9 @@
 #n-charge problem
 
+import numpy as np
+import matplotlib as plt
+
+k = 8.99e9
 class charge:
     def __init__(self, m, q, pos):
         self.mass = m
@@ -12,6 +16,34 @@ class charge:
 
     def update_velocity(self, tstep, F):
         self.velocity += (F / self.mass) * tstep
+
+    def force(self, other_charge):
+        r = other_charge.pos - self.pos
+        if np.linalg.norm(r) == 0:
+            return 0
+        F = -((k * self.charge * other_charge.charge) / (np.linalg.norm(r) ** 3)) * r
+        return F
+
+
+
+def simulate(charges, t):
+    tstep = 0.001
+    n = int(t / tstep)
+    for _ in range(n):
+        F = np.zeros(len(charges))
+        for i in range(len(charges)):
+            charges[i].update_pos(tstep) 
+            for j in range(len(charges)):
+                F[i] += charges[i].force(charges[j])
+        for i in range(len(charges)):
+            charges[i].update_pos(tstep)
+            charges[i].update_velocity(tstep, F[i])
+        
+            
+        
+        
+                
+
 
 def main():
     pass
