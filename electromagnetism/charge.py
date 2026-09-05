679cf6ca7458053b1edd41369474f6c6ffc9617e
Benjamin <benjaminboyarsky21@gmail.com>
2026-09-04 23:52:36 -0700

    Improved Efield Visual

---
+++ electromagnetism/charge.py
@@ -0,0 +1,88 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#updates: chang
+k = 8.99e9
+class Charge:
+    def __init__(self, m, q, pos):
+        self.mass = m
+        self.q = q
+        self.pos = pos
+        self.velocity = np.array([0, 0])
+
+    def update_pos(self, tstep):
+        self.pos += self.velocity * tstep
+
+    def update_velocity(self, tstep, F):
+        self.velocity += (F / self.mass) * tstep
+
+def Efield(pos, charges):
+    vector = np.zeros(2)
+    for point_charge in charges:
+        r = pos - point_charge.pos
+        vector[0] += ((k * point_charge.q) / np.linalg.norm(r) ** 2) * (r[0] / np.linalg.norm(r))
+        vector[1] += ((k * point_charge.q) / np.linalg.norm(r) ** 2) * (r[1] / np.linalg.norm(r))
+    return vector
+
+def EfieldGraph(charges):
+    Xmax = charges[0].pos[0]
+    Xmin = charges[0].pos[0]
+    Ymax = charges[0].pos[1]
+    Ymin = charges[0].pos[1]
+    for i in range(len(charges)):
+        Xpos = charges[i].pos[0]
+        Ypos = charges[i].pos[1]
+        if Xpos > Xmax:
+            Xmax = Xpos
+        elif Xpos < Xmin:
+            Xmin = Xpos
+        if Ypos > Ymax:
+            Ymax = Ypos
+        elif Ypos < Ymin:
+            Ymin = Ypos
+    max = Xmax - Xmin
+    if Ymax - Ymin > max:
+        max = Ymax - Ymin
+
+    if max == 0:
+        max = 1
+    Xbounds = [Xmin - 0.3*max, Xmax + 0.3*max]
+    Ybounds = [Ymin - 0.3*max, Ymax + 0.3*max]
+
+    n = 40
+    x = np.linspace(Xbounds[0], Xbounds[1], n)
+    y = np.linspace(Ybounds[0], Ybounds[1], n)
+    X, Y = np.meshgrid(x, y)
+    U = np.zeros_like(X)
+    V = np.zeros_like(Y)
+    for point_charge in charges:
+        for i in range(len(x)):
+            for j in range(len(y)):
+                pos = np.array([x[i], y[j]])
+                charge_pos = point_charge.pos
+                r = pos - charge_pos
+                if np.linalg.norm(r) > 0:
+                    U[j][i] += ((k * point_charge.q) / np.linalg.norm(r) ** 2) * (r[0] / np.linalg.norm(r))
+                    V[j][i] += ((k * point_charge.q) / np.linalg.norm(r) ** 2) * (r[1] / np.linalg.norm(r))
+    
+    magnitudes = np.sqrt(U**2 + V**2)
+    def magnitude(magnitudes, min_mag, max_mag):
+        return 0.002 + 0.004 * ((mag - min_mag) / (max_mag - min_mag))
+
+    fig, ax = plt.subplots()
+    for i in range(n):
+        for j in range(n):
+            mag = magnitudes[j, i]
+            ax.quiver(
+                X[j, i], Y[j, i],
+                U[j, i]/mag, V[j, i]/mag,
+                width=magnitude(mag, np.min(magnitudes), np.max(magnitudes)),
+                scale = 40
+            )
+    
+    
+    #ax.quiver(X, Y, U/magnitude, V/magnitude, magnitude, angles = 'xy', scale = 40)
+    ax.set_aspect('equal')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    plt.show()
--- electromagnetism/charge_runner.py
+++ electromagnetism/charge_runner.py
@@ -7,28 +7,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import ast
+import charge
 
 k = 8.99e9
-class charge:
-    def __init__(self, m, q, pos):
-        self.mass = m
-        self.charge = q
-        self.pos = pos
-        self.velocity = np.array([0, 0])
-
-    def update_pos(self, tstep):
-        self.pos += self.velocity * tstep
-
-    def update_velocity(self, tstep, F):
-        self.velocity += (F / self.mass) * tstep
-
-    def force(self, other_charge):
-        r = other_charge.pos - self.pos
-        if np.linalg.norm(r) == 0:
-            return 0
-        F = -((k * self.charge * other_charge.charge) / (np.linalg.norm(r) ** 3)) * r
-        return F
-
 def n_body_problem(charges, t): #unfinished
     plt.ion()
     tstep = 0.001
@@ -43,70 +24,13 @@ def n_body_problem(charges, t): #unfinished
             charges[i].update_pos(tstep)
             charges[i].update_velocity(tstep, F[i])
 
-def simulate(test_charge, charges, time):
+def simulate_w_fixed_points(test_charge, charges, time):
     tstep = 0.001
     n = int(time / tstep)
     for _ in range(n):
         test_charge.pos += test_charge.velocity * tstep
-        F = test_charge.charge * Efield(test_charge.pos, charges)
+        F = test_charge.q * charge.Efield(test_charge.pos, charges)
         test_charge.update_velocity(tstep, F)
-
-def Efield(pos, charges):
-    vector = np.zeros(2)
-    for point_charge in charges:
-        r = pos - point_charge.pos
-        vector[0] += ((k * point_charge.charge) / np.linalg.norm(r) ** 2) * (r[0] / np.linalg.norm(r))
-        vector[1] += ((k * point_charge.charge) / np.linalg.norm(r) ** 2) * (r[1] / np.linalg.norm(r))
-    return vector
-
-
-def EfieldGraph(charges):
-    Xmax = charges[0].pos[0]
-    Xmin = charges[0].pos[0]
-    Ymax = charges[0].pos[1]
-    Ymin = charges[0].pos[1]
-    for i in range(len(charges)):
-        Xpos = charges[i].pos[0]
-        Ypos = charges[i].pos[1]
-        if Xpos > Xmax:
-            Xmax = Xpos
-        elif Xpos < Xmin:
-            Xmin = Xpos
-        if Ypos > Ymax:
-            Ymax = Ypos
-        elif Ypos < Ymin:
-            Ymin = Ypos
-    max = Xmax - Xmin
-    if Ymax - Ymin > max:
-        max = Ymax - Ymin
-
-    if max == 0:
-        max = 1
-    Xbounds = [Xmin - 0.3*max, Xmax + 0.3*max]
-    Ybounds = [Ymin - 0.3*max, Ymax + 0.3*max]
-
-    n = 30
-    x = np.linspace(Xbounds[0], Xbounds[1], n)
-    y = np.linspace(Ybounds[0], Ybounds[1], n)
-    X, Y = np.meshgrid(x, y)
-    U = np.zeros_like(X)
-    V = np.zeros_like(Y)
-    for point_charge in charges:
-        for i in range(len(x)):
-            for j in range(len(y)):
-                pos = np.array([x[i], y[j]])
-                charge_pos = point_charge.pos
-                r = pos - charge_pos
-                if np.linalg.norm(r) > 0:
-                    U[j][i] += ((k * point_charge.charge) / np.linalg.norm(r) ** 2) * (r[0] / np.linalg.norm(r))
-                    V[j][i] += ((k * point_charge.charge) / np.linalg.norm(r) ** 2) * (r[1] / np.linalg.norm(r))
-
-    fig, ax = plt.subplots()
-    ax.quiver(X, Y, U, V, angles = 'xy')
-    ax.set_aspect('equal')
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    plt.show()
 
 def charge_creator(n):
     charges = []
@@ -162,22 +86,20 @@ def decipher(code):
         m.append(float(line.pop()))
     charges = []
     for i in range(len(m)):
-        charges.append(charge(m[i], q[i], pos[i]))
+        charges.append(charge.Charge(m[i], q[i], pos[i]))
     return charges
-
 
 def main():
     code = input('Do you have a code? (Y/N): ')
     if code == 'Y':
         line = input('Code: ')
         charges = decipher(line)
-        EfieldGraph(charges)
-
+        charge.EfieldGraph(charges)
 
     elif code == 'N':
         n = int(input('Number of Charges: '))
         charges = charge_creator(n)
-        EfieldGraph(charges)
+        charge.EfieldGraph(charges)
 
 if __name__ == '__main__':
     main()
